fbe98fd648150a1dc0aa7841ad86a2d471e745a4
apelsinaizomcka@mail.ru <apelsinaizomcka@mail.ru@bd3bfeb9-e807-5c44-97af-7bc5b4884173>
2013-10-07 08:42:42 +0000

    Ориентация кнопок во внутриигровом меню

--- src/panels/MenuPanel.java
+++ src/panels/MenuPanel.java
@@ -1,12 +1,13 @@
 package panels;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import main.Game;
@@ -20,15 +21,33 @@ public class MenuPanel extends JPanel
     	final boolean prevStepState = g.stepState;
     	setPreferredSize(dim);
     	
-        int bheight = (int) (dim.getHeight()/9);
-        int bwidth = (int) (dim.getWidth()/3);
-        int range = bheight;
-        setLayout(new FlowLayout(FlowLayout.CENTER, 0, range));
+    	int countOfButtons = 4;
+        int bheight = (int) (dim.getHeight()/(2*countOfButtons+1));
+        int bwidth = (int) (dim.getWidth()/2);
+        
+        int gap = (int) (dim.getWidth()-bwidth);
+        int range = (int) ((dim.getHeight() - countOfButtons * bheight)/(countOfButtons+1));
+        
+        setLayout(new FlowLayout(FlowLayout.CENTER, gap, range));
+        
+        Dimension button = new Dimension(bwidth, bheight);
+//        contin.setPreferredSize(button);
+//        contin.setMinimumSize(button);
+//        contin.setMaximumSize(button);
+//        contin.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+//        contin.setAlignmentY(JComponent.CENTER_ALIGNMENT);
     	
     	JButton continueb = new JButton("Continue");
+    	continueb.setPreferredSize(button);
+    	
     	JButton save = new JButton("Save");
+    	save.setPreferredSize(button);
+    	
     	JButton load = new JButton("Load");
+    	load.setPreferredSize(button);
+
     	JButton quit = new JButton("Quit");
+    	quit.setPreferredSize(button);
     	
     	continueb.addActionListener(new ActionListener()
 		{
